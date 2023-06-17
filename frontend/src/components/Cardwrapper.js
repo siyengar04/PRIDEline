4436778507d74f046763324e8cd06561d4f88185
Arul Aravinthan <arul@Aruls-MacBook-Air.local>
2023-06-17 16:33:59 -0700

    Removed Header

--- frontend/src/components/Cardwrapper.js
+++ frontend/src/components/Cardwrapper.js
@@ -11,6 +11,8 @@ import {
   Stack,
   Text,
   useColorModeValue,
+  Center,
+  Link,
 } from "@chakra-ui/react";
 import { ReactElement } from "react";
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
@@ -18,17 +20,24 @@ const Book = ({ title, author, description, thumbnail, infoLink }) => {
   return (
     <Card maxW="sm">
       <CardBody>
-        <Image src={thumbnail} borderRadius="lg" align={"center"} />
+        <Center>
+          <Image src={thumbnail} borderRadius="md" />
+        </Center>
         <Stack mt="6" spacing="3">
           <Heading size="md">{title}</Heading>
-          <Text width={"20vh"}>{description}</Text>
+          <Text width={"25vh"}>{description}</Text>
         </Stack>
       </CardBody>
       <Divider />
       <CardFooter>
         <ButtonGroup spacing="2">
-          <Button variant="solid" colorScheme="blue">
-            Buy now
+          <Link href={infoLink} isExternal={true}>
+            <Button variant="solid" colorScheme="blue">
+              Learn More
+            </Button>
+          </Link>
+          <Button variant="ghost" colorScheme="blue">
+            Preview
           </Button>
         </ButtonGroup>
       </CardFooter>
@@ -39,14 +48,14 @@ const Book = ({ title, author, description, thumbnail, infoLink }) => {
 export default function gridListWith() {
   return (
     <Box p={4}>
-      <Stack spacing={4} as={Container} maxW={"3xl"} textAlign={"center"}>
+      {/* <Stack spacing={4} as={Container} maxW={"3xl"} textAlign={"center"}>
         <Heading fontSize={{ base: "2xl", sm: "4xl" }} fontWeight={"bold"}>
           PrideLine
         </Heading>
         <Text color={"gray.600"} fontSize={{ base: "sm", sm: "lg" }}>
           View our LGBTQ friendly books!
         </Text>
-      </Stack>
+      </Stack> */}
 
       <Container maxW={"9xl"} mt={12}>
         <Flex flexWrap="wrap" gridGap={6} justify="center">
@@ -59,7 +68,9 @@ export default function gridListWith() {
             thumbnail={
               "http://books.google.com/books/content?id=6SQ6DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
             }
-            infoLink={"infoLink"}
+            infoLink={
+              "https://books.google.com/books/about/LGBTQ_Cultures.html?id=WZOIAQAACAAJ"
+            }
           ></Book>
           <Book
             title={"LGBTQ Cultures"}
