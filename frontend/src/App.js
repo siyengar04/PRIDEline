f9c1bdacca00e8e499550bfe08582692209d77a8
Arul Aravinthan <aravinthan@wisc.edu>
2023-06-18 01:15:00 -0700

    Added comments to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { React } from "react";
 import { ChakraProvider, Heading } from "@chakra-ui/react";
 import CardWrapper from "./components/Cardwrapper.js";
-
+//Main App component
 function App() {
   return (
     <ChakraProvider>
